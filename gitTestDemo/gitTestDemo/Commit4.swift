538b3c4b9e93b6b0e7ad080c64448bbb41eecd8f
Mike Gu <guguangzhou@didiglobal.com>
2025-10-01 21:56:07 +0800

    feature: commit4

---
+++ gitTestDemo/gitTestDemo/Commit4.swift
@@ -0,0 +1,14 @@
+//
+//  Commit4.swift
+//  gitTestDemo
+//
+//  Created by didi on 01/10/2025.
+//
+
+import UIKit
+
+class Commit4: NSObject {
+    override init() {
+        print("Commit4")
+    }
+}
