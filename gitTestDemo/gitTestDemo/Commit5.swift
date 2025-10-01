d342de2b21bef340ded3b4e11f773f079587b27d
Mike Gu <guguangzhou@didiglobal.com>
2025-10-01 22:00:44 +0800

    feature: commit5

---
+++ gitTestDemo/gitTestDemo/Commit5.swift
@@ -0,0 +1,14 @@
+//
+//  Commit5.swift
+//  gitTestDemo
+//
+//  Created by didi on 01/10/2025.
+//
+
+import UIKit
+
+class Commit5: NSObject {
+    override init() {
+        print("Commit5")
+    }
+}
